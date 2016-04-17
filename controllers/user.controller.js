30326a6b76e316ef2970cda74d822567ebf88c1f
Kimia Hassanzadeh <kim.hszd@gmail.com>
2016-04-17 15:46:05 -0600

    push

---
+++ controllers/user.controller.js
@@ -0,0 +1,47 @@
+var User = require('../models/user.model.js');
+var bcrypt = require('bcryptjs');
+
+
+module.exports.createUser = function(newUser, callback){
+	console.log('creating user ...');
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}; 
+
+module.exports.validPassword = function(candiatePassword, hash, callback){
+	bcrypt.compare(candiatePassword, hash, function(err, isMatch) {
+	    callback(null, isMatch);
+	});
+}; 
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}; 
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+};
+
+module.exports.getUsers = function(callback){
+	User.find(function (err, users) {
+	  callback(err,users);
+	})
+};
+
+module.exports.filterByGroup = function(req,res){
+	var query = User.find();
+	var filter = req.body.group;
+	if(filter.length>0)
+    {
+    	query.where({group:filter});
+    }  
+    query.exec(function(err, results){
+    	if(err) throw err;
+    	res.render('index', { title: 'Members', allUsers:results})
+    });
+
+}
---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db= mongoose.connection;
+
+mongoose.connect('mongodb://demo:pass@ds023510.mlab.com:23510/megabat');
+
+var userSchema= new Schema({
+	name : {
+		type: String,
+		index: true,
+	},
+	username : {
+		type: String
+	},
+	email : {
+		type: String
+	},
+	password : {
+		type: String
+	},
+	profilepicture : {
+		type:String
+	},
+	joinedOn:{
+		type:Date,
+		default: Date.now
+	},
+	group: {
+		type: String
+	}
+});
+
+var User = module.exports = mongoose.model('User', userSchema);
